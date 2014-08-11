a447d8fc3143c1eb5f0fae587934503a3038d05b
Luke Phillips <luke@femurdesign.com>
2014-08-11 14:58:03 +0100

    added audio to bouncing ball

--- natureofcode/vectors/bouncingBall.ts
+++ natureofcode/vectors/bouncingBall.ts
@@ -1,14 +1,17 @@
 import Sketch = require("../../../src/sketch");
 import PVector = require("../../../src/pvector");
+import SoundGen = require("../../../src/audio");
 
 class BouncingBall extends Sketch{
 
 	position: PVector;
 	velocity: PVector;
+    audio: SoundGen;
 
     setup(): void{
     	this.position = new PVector(100, 100);
   		this.velocity = new PVector(2.5, 5);
+        this.audio = new SoundGen();
     }
 
     draw(){
@@ -18,10 +21,13 @@ class BouncingBall extends Sketch{
 
 		if ((this.position.x > width) || (this.position.x < 0)) {
 			this.velocity.x = this.velocity.x * -1;
+            this.audio.bleep(440);
+            this.audio.playNote();
 		}
 
 		if ((this.position.y > height) || (this.position.y < 0)) {
 			this.velocity.y = this.velocity.y * -1;
+            this.audio.bleep(660);
 		}
 
 		fill(0);
